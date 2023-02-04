b0bd84a7b4582501922518da54ec085da5cceca7
Megan Korling <korlingm@gmail.com>
2023-02-04 13:21:34 -0800

    Add state to form

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,19 +1,66 @@
-import { useState } from "react";
+import { useState } from 'react';
+
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
-    const [location, setLocation] = useState("Seattle, WA");
+    const [location, setLocation] = useState('Seattle, WA');
+    const [animal, updateAnimal] = useState("");
+    const [breed, updateBreed] = useState("");
+    const breeds = ["poodle"];
 
     return (
         <div className="search-params">
             <form>
                 <label htmlFor="location">
                     Location
-                    <input id="location" value={location} placeholder="Location" onChange={(e) => setLocation(e.target.value)}/>
+                    <input 
+                        id="location" 
+                        value={location} 
+                        placeholder="Location" 
+                        onChange={(e) => setLocation(e.target.value)}/>
+                </label>
+                <label htmlFor='animal'>
+                    Animal
+                    <select 
+                        id='"animal'
+                        value={animal}
+                        onChange={(e) => {
+                            updateAnimal(e.target.value); 
+                            updateBreed("");
+                        }}
+                        // onBlur={(e) => {
+                        //     updateAnimal(e.target.value);
+                        //     updateBreed("");
+                        // }}
+                    >
+                    <option/>
+                        {ANIMALS.map((animal) => (
+                            <option key={animal} value={animal}>{animal}</option>
+                        ))}
+                        
+                    </select>
+                </label>
+                <label htmlFor="breed">
+                    Breed
+                    <select
+                        disabled={!breeds.length}
+                        id="breed"
+                        value={breed}
+                        onChange={(e) => updateBreed(e.target.value)}
+                        // onBlur={(e) => updateBreed(e.target.value)}
+                    >
+                    <option />
+                        {breeds.map((breed) => (
+                            <option key={breed} value={breed}>
+                                {breed}
+                            </option>
+                        ))}
+                    </select>
                 </label>
                 <button>Submit</button>
             </form>
         </div>
-    )
-}
+    );
+};
 
 export default SearchParams;
