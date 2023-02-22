3215d5c9dd9c7857dc2a1dec53f8204ebde541ac
Megan Korling <korlingm@gmail.com>
2023-02-22 13:37:28 -0800

    Add button functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import {createRoot} from 'react-dom/client';
+import { useState } from 'react';
 import SearchParams from './SearchParams';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Details from './Details';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AdoptedPetContext from './AdoptedPetContext';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -15,18 +17,21 @@ const queryClient = new QueryClient({
 
 // will be rendered once
 const App = () => {
+  const adoptedPet = useState(null);
   // type of tag, {} can carry attributes/props like ids, any children elements
   return (
     <div>
       <BrowserRouter>
         <QueryClientProvider client={queryClient}>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
-          <Routes>
-            <Route path="/details/:id" element={<Details />} />
-            <Route path="/" element={<SearchParams />} />
-          </Routes>
+          <AdoptedPetContext.Provider value={adoptedPet}>
+            <header>
+              <Link to="/">Adopt Me!</Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+            </Routes>
+          </AdoptedPetContext.Provider>
         </QueryClientProvider>
       </BrowserRouter>
     </div>
--- src/Details.jsx
+++ src/Details.jsx
@@ -1,13 +1,20 @@
-import { useParams } from "react-router";
+import { useParams, useNavigate } from "react-router";
 import { useQuery } from "@tanstack/react-query";
+import { useState, useContext } from "react";
 import fetchPet from "./fetchPet";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
+import Modal from "./Modal";
+import AdoptedPetContext from "./AdoptedPetContext";
 
 const Details = () => {
     const {id} = useParams();
     // if details with id of 1 (for example) isn't in cache, it will call fetchPet
     const results = useQuery(["details", id], fetchPet);
-    
+    const [showModal, setShowModal] = useState(false);
+    const navigate = useNavigate();
+    const [_, setAdoptedPet] = useContext(AdoptedPetContext);
+
     if (results.isLoading) {
         return (
             <div className="loading-pane">
@@ -24,11 +31,34 @@ const Details = () => {
             <div>
                 <h1>{pet.name}</h1>
                 <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
-                <button>Adopt {pet.name}</button>
+                <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
                 <p>{pet.description}</p>
+                {
+                    showModal ? (
+                        <Modal>
+                            <div>
+                                <h1>Would you like to adopt {pet.name}?</h1>
+                                <div className="buttons">
+                                <button onClick={() => {
+                                    setAdoptedPet(pet); 
+                                    navigate('/');
+                                }}>Yes</button>
+                                <button onClick={() => setShowModal(false)}>No</button>
+                                </div>
+                            </div>
+                        </Modal>
+                    ) : null 
+                }
             </div>
         </div>
     );
 };
 
-export default Details;
+function DetailsErrorBoundary(props) {
+    return (
+        <ErrorBoundary>
+            <Details {...props}/>
+        </ErrorBoundary>
+    )
+}
+export default DetailsErrorBoundary;
